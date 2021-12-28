eb064915195dd12d42dd3b30e86ceef9a11b3219
Krzysztof Solecki <32575999+krzychsol@users.noreply.github.com>
2021-12-28 19:54:09 +0100

    Delete EvolutionSimulator directory

--- EvolutionSimulator/src/main/java/agh/ics/darwin/Map/IEnergyChangeObserver.java
+++
@@ -1,7 +0,0 @@
-package agh.ics.darwin.Map;
-
-import agh.ics.darwin.Objects.Animal;
-
-public interface IEnergyChangeObserver {
-    void energyChanged(Animal animal);
-}
--- EvolutionSimulator/src/main/java/agh/ics/darwin/Map/IPositionChangeObserver.java
+++
@@ -1,8 +0,0 @@
-package agh.ics.darwin.Map;
-
-import agh.ics.darwin.Objects.Animal;
-import agh.ics.darwin.Math.Vector2d;
-
-public interface IPositionChangeObserver {
-    void positionChanged(Vector2d oldPosition, Vector2d newPosition, Animal animal);
-}
