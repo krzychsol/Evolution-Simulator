945870e7c2d6888d4b7e2dfdc6c0b7a81cc0c6ff
Krzysztof Solecki <32575999+krzychsol@users.noreply.github.com>
2021-12-28 19:52:59 +0100

    Delete Darwin_World-master directory

--- Darwin_World-master/src/main/agh/cs/darwinProject/Map/IEnergyChangeObserver.java
+++
@@ -1,7 +0,0 @@
-package agh.cs.darwinProject.Map;
-
-import agh.cs.darwinProject.Objects.Animal;
-
-public interface IEnergyChangeObserver {
-    void energyChanged(Animal animal);
-}
--- Darwin_World-master/src/main/agh/cs/darwinProject/Simulation/IAnimalDeathObserver.java
+++
@@ -1,7 +0,0 @@
-package agh.cs.darwinProject.Simulation;
-
-import agh.cs.darwinProject.Objects.Animal;
-
-public interface IAnimalDeathObserver {
-    void animalDied(Animal animal);
-}
